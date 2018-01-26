45b61177a1c25c81705064d712ce20f2d7087cb0
Maria Brumm <maria.brumm@seattlechildrens.org>
2018-01-26 11:08:48 -0800

    Match list view + stub of match review view

--- mysite/matcher/urls.py
+++ mysite/matcher/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('review/<int:match_id>/', views.review_match, name='review match'),
 ]
 
 
--- mysite/matcher/views.py
+++ mysite/matcher/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+from matcher.models import PatientRecord, RecordMatch
 
 # Create your views here.
 
 # index is also the match overview, because why not
 def index(request):
-    return HttpResponse("Hello world, this is the web app.")
+
+    match_list = RecordMatch.objects.order_by('-score')
+    template = loader.get_template('matcher/index.html')
+    context = {
+            'match_list': match_list,
+            }
+    return HttpResponse(template.render(context, request))
 
 # 
 def review_match(request, match_id):
@@ -13,4 +22,5 @@ def review_match(request, match_id):
     # update the status of the previous match ID,
     # then display the highest-scoring unreviewed match
 
-    return HttpResponse("This is the review page for RecordMatch %s.")
+
+    return HttpResponse("This is the review page for RecordMatch %s." % match_id)
